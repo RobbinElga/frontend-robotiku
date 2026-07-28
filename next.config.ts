625e36ed119bfccef14ea70a5df79d0695f8bc0e
Elga <elgafirmantara27@gmail.com>
2026-07-28 15:25:19 +0700

    login

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  async rewrites() {
-    return [
-      { source: "/api/:path*", destination: "http://localhost:8000/api/:path*" },
-      { source: "/storage/:path*", destination: "http://localhost:8000/storage/:path*" },
-    ];
-  },
-  // isi dengan host tunnel-mu nanti (lihat langkah 4), mis. "xxxx-3000.asse.devtunnels.ms"
-  allowedDevOrigins: ["https://6gjbc12h-3000.asse.devtunnels.ms/"],
+const isDev = process.env.NODE_ENV === "development";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  ...(isDev
+    ? {
+      async rewrites() {
+        return [
+          { source: "/api/:path*", destination: "http://localhost:8000/api/:path*" },
+          { source: "/storage/:path*", destination: "http://localhost:8000/storage/:path*" },
+        ];
+      },
+      allowedDevOrigins: ["https://6gjbc12h-3000.asse.devtunnels.ms"],
+    }
+    : {}),
 };
 
 export default nextConfig;
